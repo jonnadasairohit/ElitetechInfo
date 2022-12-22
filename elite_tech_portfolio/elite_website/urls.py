78beb1de1cbc342d507bfc90fd703aa9c7a647e0
jonnadasairohit <jonnadasairohit@gmail.com>
2022-12-21 18:13:16 -0600

    Changes done for index and urls.py

--- elite_tech_portfolio/elite_website/urls.py
+++ elite_tech_portfolio/elite_website/urls.py
@@ -3,6 +3,6 @@ from elite_website import views
 
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('contact-us/', views.contactus, name='contact-us'),
                ]
